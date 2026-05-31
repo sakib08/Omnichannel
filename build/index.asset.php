26fecbee4adea1447a47e49d07bf0b05e2cba046
sakib08 <sakib.bd08@gmail.com>
2026-05-31 16:36:02 +0600

    telegram check and help page added

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '449a5d277e3ea5cd252a');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => 'e8c24dedce931e7e79ba');
